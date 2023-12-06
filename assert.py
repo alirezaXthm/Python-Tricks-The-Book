56d2cdea214b8a13252055be38eeab29ae4c8339
alireza tahami <alireza.thm03@gmail.com>
2023-12-06 17:28:22 +0330

    assert trick

--- assert.py
+++ assert.py
@@ -1,8 +1,16 @@
-age = int(input())
-
+age = int(input('How old are you?'))
+gender = input('Gender (M/F)?')
 
 def age_validation(age):
     assert age > 18 , 'you are under 18'
     return True
-
+        
 print(age_validation(age))
+
+if gender == 'M':
+    print('you are a man!')
+elif gender == 'F':
+    print('sorry this is for men :)')
+else:
+    assert False, f'what the fuck is the "{gender}" gender?'
+        
